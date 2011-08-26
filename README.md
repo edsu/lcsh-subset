ca4a896c4e2019ebff50df26c93783f78bb5191c
Ed Summers <ehs@pobox.com>
2011-08-26 12:40:10 -0400

    working with the new opensearch api

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-Sometimes it might be handy to have a working subset of LCSH for some
-applications. This command line program uses the OpenSearch API at
-id.loc.gov to do a query, page through the results and build a graph
-of the SKOS concepts.
+Sometimes you might want to have a working subset of the [Library of Congress Subject Headings](http://en.wikipedia.org/wiki/Library_of_Congress_Subject_Headings) for some SKOS driven applications.
+
+For example at the [Library of Congress](http://loc.gov) there is a government
+policy web archive, where it is desirable to catalog portions of the collection
+using the [HIVE software](http://code.google.com/p/hive-mrc/), which can load
+SKOS RDF.
+
+This command line program uses the OpenSearch API at
+id.loc.gov to do a query, page through the results, build up a graph
+of the SKOS concepts, and output the graph as RDF/XML.
+
+For example:
+
+    % ./lcsh-subset.py "government policy"
+
+dumps all the SKOS RDF for concepts mentioning beer to the file:
+
+    government_policy.rdf
+
+
+License: Public Domain
+Author: [Ed Summers](mailto:ehs@pobox.com)
--- lcsh-subset.py
+++ lcsh-subset.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 
 import sys
+import urllib
+
 import rdflib
 import feedparser
 
-q = sys.argv[1]
-page = 0
+# get the query 
+q = urllib.quote(sys.argv[1])
+
+# the graph to persist the concepts to
 graph = rdflib.Graph()
 
+# use OpenSearch Atom to aggregate the RDF for the search results
+start = 0
 while True:
 
-    page += 1
+    # TODO: the awkward 2nd q param for scoping to lcsh will hopefully 
+    # be changed to be a bit simpler...till then this looks ugly
+    url = "http://id.loc.gov/search/?q=%s&format=atom&start=%i&q=cs%%3Ahttp%%3A%%2F%%2Fid.loc.gov%%2Fauthorities%%2Fsubjects" % (q, start)
 
-    url = "http://id.loc.gov/search/?q=%s&page=1&format=atom&cs=cs%%3Alcsh&page=%i" % (q, page)
-
-    print url
+    # fetch the atom feed
     feed = feedparser.parse(url)
 
+    # no need to continue if there aren't any hits
     if len(feed.entries) == 0:
         break
 
+    # load the rdf for each hit
     for entry in feed.entries:
+        start += 1
         for link in entry.links:
             if link.type == "application/rdf+xml":
-                print entry.title, link.href
+                print "loading %s <%s>" % (entry.title, link.href)
                 graph.load(link.href)
 
-graph.serialize(file("subset.rdf", "w"))
+# output the resulting graph as RDF/XML
+filename = "%s.rdf" % sys.argv[1].replace(" ", "_")
+
+graph.serialize(file(filename, "w"))
